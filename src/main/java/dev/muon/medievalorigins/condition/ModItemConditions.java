800e31d814a90f29ee2f3bb16901d52a23da65ff
muon-rw <pashah012@gmail.com>
2024-06-02 20:53:04 -0500

    Clean up

--- src/main/java/dev/muon/medievalorigins/condition/ModItemConditions.java
+++ src/main/java/dev/muon/medievalorigins/condition/ModItemConditions.java
@@ -4,12 +4,9 @@ import dev.muon.medievalorigins.MedievalOrigins;
 import io.github.apace100.apoli.power.factory.condition.ConditionFactory;
 import io.github.apace100.apoli.registry.ApoliRegistries;
 import io.github.apace100.calio.data.SerializableData;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.*;
 import net.minecraft.core.Registry;
 import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.item.enchantment.Enchantments;
 
 public class ModItemConditions {
     public static void register() {
@@ -22,19 +19,6 @@ public class ModItemConditions {
             }
             return false;
         }));
-
-        // Switched to AutoTag for performance
-        /*
-        register(new ConditionFactory<>(MedievalOrigins.loc("is_weapon"), new SerializableData(), (data, stack) ->
-                Enchantments.SHARPNESS.canEnchant(stack) || stack.getItem() instanceof BowItem || stack.getItem() instanceof DiggerItem && ((DiggerItem) stack.getItem()).getAttackDamage() > 0));
-
-        register(new ConditionFactory<>(MedievalOrigins.loc("is_bow"), new SerializableData(), (data, stack) ->
-                stack.getItem() instanceof BowItem));
-
-        register(new ConditionFactory<>(MedievalOrigins.loc("is_dagger"), new SerializableData(), (data, stack) -> {
-            Item item = stack.getItem();
-            return item instanceof SwordItem && BuiltInRegistries.ITEM.getKey(item).getPath().matches("[a-z_]*(dagger|sai|knife)[a-z_]*");
-        }));*/
 
     }
 
