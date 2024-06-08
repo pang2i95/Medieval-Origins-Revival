cdb884cda98521157cdb3ac32c119559267beb66
muon-rw <pashah012@gmail.com>
2024-06-08 02:48:22 -0500

    Cast spell action (fabric)

--- fabric/src/main/java/dev/muon/medievalorigins/action/CastSpellAction.java
+++ fabric/src/main/java/dev/muon/medievalorigins/action/CastSpellAction.java
@@ -12,22 +12,22 @@ import net.minecraft.world.item.ItemStack;
 import net.spell_engine.internals.SpellHelper;
 
 public class CastSpellAction {
-    public static void castAction(Player player, String spellIdentifier) {
-        ItemStack itemStack = player.getMainHandItem();
-        ResourceLocation spellID = new ResourceLocation(spellIdentifier);
-        SpellHelper.attemptCasting(player, itemStack, spellID, false);
-    }
     public static void action(SerializableData.Instance data, Entity caster) {
-}
+        if (caster instanceof Player) {
+            Player player = (Player) caster;
+            ItemStack itemStack = player.getMainHandItem();
+            SpellHelper.attemptCasting(player, itemStack, data.getId("spell"), data.getBoolean("require_ammo"));
+        }
+        // todo: run bientity action. pls do this future me ty
+    }
 
     public static ActionFactory<Entity> getFactory() {
         return new ActionFactory<>(
                 MedievalOrigins.loc("cast_spell"),
                 new SerializableData()
                         .add("spell", SerializableDataTypes.IDENTIFIER)
-                        .add("tag", SerializableDataTypes.NBT, null)
-                        .add("entity_action", ApoliDataTypes.ENTITY_ACTION, null)
-                        .add("bientity_action", ApoliDataTypes.BIENTITY_ACTION, null),
+                        .add("bientity_action", ApoliDataTypes.BIENTITY_ACTION, null)
+                        .add("require_ammo", SerializableDataTypes.BOOLEAN, null),
                 CastSpellAction::action
         );
     }
